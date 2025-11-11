c76fa0d253cb6fde48c418cd8f70efc5fbfdbc4d
ivo448 <ignacioagustinvo@gmail.com>
2025-11-11 04:38:54 -0300

    asdas

--- panol/settings.py
+++ panol/settings.py
@@ -51,11 +51,6 @@ if IS_PRODUCTION:
             'PASSWORD': DB_PASS,
             'HOST': DB_HOST,
             'PORT': DB_PORT,
-            'OPTIONS': {
-                'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-                'charset': 'utf8mb4',
-                'options': {'_auth_data_func': lambda: None} 
-            },
         }
     }
 
